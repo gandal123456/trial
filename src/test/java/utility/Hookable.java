d3728a2ad1cbd515e9ff27a98024801bd79f1833
gandal123456 <ganeshgandal@gmail.com>
2021-09-29 09:36:44 +0530

    by ganesh

---
+++ src/test/java/utility/Hookable.java
@@ -0,0 +1,46 @@
+package utility;
+
+
+
+import java.net.UnknownHostException;
+
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import utility.HTMLReportGenerator;
+
+public class Hookable 
+{
+	
+	
+	   @Before
+	   public void before(Scenario scenario) throws UnknownHostException
+	   {
+		   HTMLReportGenerator.TestSuiteStart("D:\\report1\\flipkart.html", "Flipkart");
+		   
+		   HTMLReportGenerator.TestCaseStart(scenario.getName(), scenario.getStatus());
+		   
+		   System.out.println("--------------------------------Scenario start----------------------------------------");
+	   }
+	
+	
+	
+	
+	
+       @After
+	   public void after(Scenario scenario)
+	   {
+		   System.out.println("--------------------------------Scenario end----------------------------------------");
+		   
+		   HTMLReportGenerator.TestCaseEnd();
+		   HTMLReportGenerator.TestSuiteEnd();
+	   }
+	
+	
+	
+	
+	
+	
+	
+
+}
